a8e53066b5431d7f6d5ef1dd31dce2bac4f0b155
Marco Grier <mlgrier@gmail.com>
2017-09-20 20:41:34 -0500

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Marco Grier on 9/20/17.
+//  Copyright © 2017 Marco Grier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
